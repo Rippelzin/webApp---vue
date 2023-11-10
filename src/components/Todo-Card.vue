620f59dffcb116f4f0c6f7592e2a4fb67afced5c
Rippelzin <gabrielrippelima@gmail.com>
2023-11-10 01:05:19 -0300

    first commit

---
+++ src/components/Todo-Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page>
+    <div v-for="item in list" :key="item.id">
+      <p :class="{ feito: item.completed }">{{ item.id }} - {{ item.title }}</p>
+    </div>
+  </q-page>
+</template>
+<script>
+import axios from "axios";
+import { list } from "postcss";
+export default {
+  props: {
+    list: Object,
+  },
+};
+</script>
+
+<style>
+.feito {
+  background-color: green;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page class="flex flex-center">
+    <TodoCard :list="list" :teste="teste" />
+  </q-page>
+</template>
+
+<script>
+/* eslint-disable */
+import { defineComponent } from "vue";
+import axios from "axios";
+import TodoCard from "src/components/Todo-Card.vue";
+
+export default defineComponent({
+  name: "IndexPage",
+
+  data() {
+    return {
+      list: [],
+      teste: "teste",
+    };
+  },
+  components: {
+    TodoCard,
+  },
+  mounted() {
+    this.fetchData();
+    console.log("aa");
+    console.log(this.list);
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos")
+        .then((response) => (this.list = response.data));
+    },
+  },
+});
+</script>
